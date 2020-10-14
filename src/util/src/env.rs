b4139d2ef59b60276e93dba302ceb0a9f8e54f57
Matthew Bayne <baynematthew509@gmail.com>
2020-10-14 16:48:04 -0500

    Environment detection.

--- src/util/src/env.rs
+++ src/util/src/env.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::io;
 use std::path::PathBuf;
+use std::process::Command;
+
 
 pub fn get_cwd() -> io::Result<PathBuf> {
     let mut pwd = env::current_exe()?;
@@ -12,15 +14,41 @@ pub fn get_main() -> io::Result<PathBuf> {
     let exe = env::current_exe()?;
     Ok(exe)
 }
-pub fn get_system(){
-    let mut os = 0;
-    let mut version = 0;
-    match env::consts::OS {
-    "linux" => os = 1,
-    "windows" => os = 2,
-    "macos" => os = 3,
-    _ => os = 4
-    };
 
-    println!("Operating System Code: {}", os);
+pub fn get_system() {
+    let mut os = 0;
+    let mut version = None;
+    let nomatch = &[0];
+    match env::consts::OS {
+        "linux" => {
+            os = 1;
+            version = Some(Command::new("uname")
+                .arg("-r")
+                .output()
+                .expect("Failed to find Linux kernel version"));
+        }
+        "windows" => {
+            os = 1;
+            version = Some(Command::new("systeminfo")
+                .arg("| findstr /B /C:\"OS Name\" /C:\"OS Version\"")
+                .output()
+                .expect("Failed to find Windows version"));
+
+        }
+        "macos" => {
+            os = 3;
+            version = Some(Command::new("uname")
+                .arg("-r")
+                .output()
+                .expect("Failed to find MacOS version"));
+            
+        }
+        _ => os = 4,
+    };
+    let _version = 
+    match &version {
+        Some(p) => String::from_utf8_lossy(&p.stdout),
+        None => String::from_utf8_lossy(nomatch)
+    };
+    println!("OS = {} Version = {}", os, _version);
 }
